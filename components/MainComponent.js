d860253665a1b9f4c66001651c99d91e6cc268cd
TianyouLi <litianyou97@gmail.com>
2020-10-24 18:10:05 -0400

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,28 +2,44 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
-import {View} from 'react-native';
+import {View, Platform} from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack'; // createStackNavigator has been deleted from react-navigation v4.0 and moved into react-navigation-stack
+import { createAppContainer } from 'react-navigation';
+
+
+const MenuNavigator = createStackNavigator({
+  Menu: { screen: Menu },
+  Dishdetail: { screen: Dishdetail }
+},
+{
+  initialRouteName: 'Menu',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: "#fff"            
+      }
+  }
+}
+);
+
+// from react-navigation v3.0, createStackNavigator is never wrapped by a 'navigation container'
+// MUST declare a container manually
+const MN = createAppContainer(MenuNavigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
-
-  onDishSelect(dishID) {
-    this.setState({selectedDish: dishID});
-  }
 
   render() {
  
     return (
-      <View>
+      <View style={{flex: 1, paddingTop: Platform.OS==='android'?0:10 }}>
 
-        <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetail dish={this.state.dishes.filter(dish => dish.id===this.state.selectedDish)[0]} />
+        {/* <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+        <Dishdetail dish={this.state.dishes.filter(dish => dish.id===this.state.selectedDish)[0]} /> */}
+        {/* <MenuNavigator /> */}
+        <MN />
       </View>
     );
   }
