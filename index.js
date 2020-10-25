e841223d8e9a0d44362a94ec978307bfe01629e8
TianyouLi <litianyou97@gmail.com>
2020-10-24 22:01:34 -0400

    Navigation Part 3

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import App from './App';
 import {name as appName} from './app.json';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+// This solves the side slide problem. Must import gesture handler manually
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
