cd95bef8505c4aa3432240c1390d6b473e026869
TianyouLi <litianyou97@gmail.com>
2020-10-29 15:51:57 -0400

    Gestures Part 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -32,9 +32,14 @@ function RenderDish(props) {
             return false;
     };
 
+    handleViewRef = ref => this.view = ref;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
@@ -56,6 +61,7 @@ function RenderDish(props) {
     if (dish != null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                ref={this.handleViewRef}
                 {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
