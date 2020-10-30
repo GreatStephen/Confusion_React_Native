85a8415f8c5c40c11aac631578c04722acb8b6be
TianyouLi <litianyou97@gmail.com>
2020-10-30 03:15:53 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,11 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Aler
 import { Card } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker' // new version of datetimepicker
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import * as Notifications from 'expo-notifications';
+// import { Permissions, Notifications } from 'expo';
+
+
 
 class Reservation extends Component {
 
@@ -31,7 +36,11 @@ class Reservation extends Component {
             'Number of Guests: '+ this.state.guests + '\nSmoking? ' + this.state.smoking + '\nDate and Time: ' + this.state.date.toISOString(),
             [
             {text: 'Cancel', onPress: () => {this.resetForm();}, style: 'cancel'},
-            {text: 'OK', onPress: () => {this.resetForm();}},
+            {text: 'OK', onPress: () => {
+                this.presentLocalNotification(this.state.date);
+                this.resetForm();
+                }
+            },
             ],
             { cancelable: false }
         );
@@ -47,6 +56,33 @@ class Reservation extends Component {
             // showModal: false
         });
 
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
     }
 
     showDatepicker = () => {
