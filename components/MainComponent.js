9fc3571b5f2c1f3ff197a6ecad76d86803c012bd
TianyouLi <litianyou97@gmail.com>
2020-10-30 02:40:46 -0400

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,8 @@ import About from './AboutComponent';
 import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
+
 import { View, Platform, Image, StyleSheet, SafeAreaView, ScrollView, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack'; // createStackNavigator has been deleted from react-navigation v4.0 and moved into react-navigation-stack
 import { createAppContainer } from 'react-navigation';
@@ -132,6 +134,23 @@ const FavoritesNavigator = createStackNavigator({
   })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+  }, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.toggleDrawer() } />    
+  })
+});
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -149,6 +168,21 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+  Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
   Home:
   {
     screen: HomeNavigator,
@@ -241,6 +275,7 @@ const MainNavigator = createDrawerNavigator({
   }
 
 }, {
+  initialRouteName:'Home',
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
